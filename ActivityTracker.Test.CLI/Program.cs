eb1a20ef6659013d3a6e973f76ad4e748a7fbcd7
Erick Jung <eojb@PRAIADOTUPE-5.local>
2017-09-14 17:11:45 -0300

    update cli

--- ActivityTracker.Test.CLI/Program.cs
+++ ActivityTracker.Test.CLI/Program.cs
@@ -1,28 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using ActivityTracker.OSX;
+using Newtonsoft.Json;
 
 namespace ActivityTracker.Test.CLI
 {
     class Program
     {
+        private const string OutputFile = "out.json";
+
+        private static void SaveSnapshot(Snapshot snap, string outFile)
+        {
+            if (File.Exists(outFile))
+            {
+                var json = File.ReadAllText(outFile);
+                var newSnap = JsonConvert.DeserializeObject<List<Snapshot>>(json);
+                newSnap.Add(snap);
+                File.WriteAllText(outFile, JsonConvert.SerializeObject(newSnap));
+                return;
+            }
+
+            var list = new List<Snapshot> {snap};
+            File.WriteAllText(outFile, JsonConvert.SerializeObject(list));
+        }
+
         static void Main()
         {
-            var t = new Tracker();
-            var snapShot = t.Now();
-
-            Console.WriteLine("--- ALL PROCESS AND WINDOWS AT {0} ---", snapShot.Time);
-            foreach (var snap in snapShot.Processes)
-            {
-                Console.WriteLine("PROC: {0} - {1}", snap.Value.Id, snap.Value.Name);
-                
-                foreach (var win in snap.Value.Windows)
-                {
-                    Console.WriteLine("    - {0} - {1}", win.Value.Id, win.Value.Name);
-                }
-            }
-            Console.WriteLine("\n--- ACTIVE WINDOW ---");
-            Console.WriteLine("{0}", snapShot.ActiveWindow);
-            Console.WriteLine("------");
+            var t = new Tracker();    
+            var snap = t.Now();
+            SaveSnapshot(snap, OutputFile);
+            Console.WriteLine("Processes saved at {0}", snap.Time);
         }
     }
 }
