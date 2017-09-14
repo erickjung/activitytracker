889797f74869528f590626cd4c58a0ab73cbd638
Erick Jung <eojb@PRAIADOTUPE-5.local>
2017-09-14 16:19:03 -0300

    new simple cli

---
+++ ActivityTracker.Test.CLISimple/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ActivityTracker.OSX;
+
+namespace ActivityTracker.Test.CLISimple
+{
+    class Program
+    {
+        static void Main()
+        {
+            var t = new Tracker();
+            var snapShot = t.Now();
+
+            Console.WriteLine("--- ALL PROCESS AND WINDOWS AT {0} ---", snapShot.Time);
+            foreach (var snap in snapShot.Processes)
+            {
+                Console.WriteLine("PROC: {0} - {1}", snap.Value.Id, snap.Value.Name);
+                
+                foreach (var win in snap.Value.Windows)
+                {
+                    Console.WriteLine("    - {0} - {1}", win.Value.Id, win.Value.Name);
+                }
+            }
+            Console.WriteLine("\n--- ACTIVE WINDOW ---");
+            Console.WriteLine("{0}", snapShot.ActiveWindow);
+            Console.WriteLine("------");
+        }
+    }
+}
