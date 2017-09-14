5076e38d048e690176bfe39f0bad88badf9df886
Erick Jung <eojb@PRAIADOTUPE-5.local>
2017-09-14 10:06:54 -0300

    update test

--- ActivityTracker.Test.CLI/Program.cs
+++ ActivityTracker.Test.CLI/Program.cs
@@ -10,24 +10,18 @@ namespace ActivityTracker.Test.CLI
             var t = new Tracker();
             var snapShot = t.Now();
 
-            Console.WriteLine("--- ALL PROCESS ---");
+            Console.WriteLine("--- ALL PROCESS AND WINDOWS ---");
             foreach (var snap in snapShot.Processes)
             {
-                Console.WriteLine(string.Format("{0} - {1}", snap.Value.Id, snap.Value.Name));
-            }
-            Console.WriteLine("------");
-            Console.WriteLine("--- ACTIVE WINDOW ---");
-            Console.WriteLine(string.Format("{0}", snapShot.ActiveWindow));
-            Console.WriteLine("------");
-
-            Console.WriteLine("--- ALL WINDOWS ---");
-            foreach (var snap in snapShot.Processes)
-            {
+                Console.WriteLine("PROC: {0} - {1}", snap.Value.Id, snap.Value.Name);
+                
                 foreach (var win in snap.Value.Windows)
                 {
-                    Console.WriteLine(string.Format("{0} - {1} -> {2} - {3}", snap.Value.Id, snap.Value.Name, win.Value.Id, win.Value.Name));
+                    Console.WriteLine("    - {0} - {1}", win.Value.Id, win.Value.Name);
                 }
             }
+            Console.WriteLine("\n--- ACTIVE WINDOW ---");
+            Console.WriteLine("{0}", snapShot.ActiveWindow);
             Console.WriteLine("------");
         }
     }
